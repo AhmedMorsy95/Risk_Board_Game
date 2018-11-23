cb15ce943c395474d4a1925c4d375b755e4c3683
NevineSaid <44064365+NevineSaid@users.noreply.github.com>
2018-11-24 00:00:12 +0200

    added Greedy and AIPlayer

---
+++ src/risk/AIPlayer.java
@@ -0,0 +1,121 @@
+package risk;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
+import javafx.scene.paint.Color;
+
+public abstract class AIPlayer {
+     boolean turn;
+     int soldiers_of_each_territory[]  = new int[55];
+     ArrayList<Integer> territories;
+     int soldiers;
+     Color c;
+     
+     int getBonusSoldiers() {
+    	  return Math.max(territories.size()/3, 3);
+     }
+     ArrayList<Integer> getTerritories(){
+    	 return territories;
+     }
+     int getSoldiers() {
+    	 return soldiers;
+     }
+     int getSoldiers_from_territory(int i) {
+    	 return soldiers_of_each_territory[i];
+     }
+     boolean isMine(int country) {
+    	 for(int i=0;i<territories.size();i++) {
+    		 if(territories.get(i) == country) {
+    			 return true;
+    		 }
+    	 }
+    	 return false;
+     }
+     
+     Color getColor() {
+    	 return c;
+     }
+     boolean isMyTurn() {
+    	 return turn;
+     }
+     void alterTurn() {
+    	 turn = !turn;
+     }
+     void addSoldiers(int country , int x) {
+    	 soldiers += x;
+    	 soldiers_of_each_territory[country] += x; 
+     }
+     void removeSoldiers(int country , int x) {
+    	 soldiers -= x;
+    	 soldiers_of_each_territory[country] -= x; 
+     } 
+     void addTerritory(int country,int x) {
+    	 territories.add(country);
+    	 soldiers_of_each_territory[country] = x; 
+     }
+     void removeTerritory(int country) {
+    	 soldiers_of_each_territory[country] = 0;
+    	 for(int i=0;i<territories.size();i++) {
+    	     if(territories.get(i) == country)
+    	    	 territories.remove(i);
+    	 }
+     }
+     void fightStart(player one,player two,int a,int b,int from,int to) {
+    	 // starts fight where from attack to with a soldiers and to defends with b soldiers
+    	 boolean result = fight(a,b);
+    	 //System.out.print(from + " " + to + " " + a + " " + b + " " + result + "\n");
+    	 if(result) {
+    		 two.removeSoldiers(to, b);
+    		 if(two.soldiers_of_each_territory[to] == 0) { 
+    		   	 /// remove it from the loser
+    			 two.removeTerritory(to);
+    			 /// add to to winner and add 1 soldier there
+    			 one.addTerritory(to, 1);
+    			 one.removeSoldiers(from, 1);
+    		 }
+    	 }
+    	 else {
+    		 /// attackers soldiers die
+    		 one.removeSoldiers(from, a);
+    	 }
+     }
+     boolean fight(int a,int b) { /// determines if i win the fight , simulates dice throwing
+    	 ArrayList<Integer> x =  new ArrayList<Integer>();
+    	 ArrayList<Integer> y =  new ArrayList<Integer>();
+    	 
+    	 for(int i=0;i<a;i++) {
+    		 Random r = new Random();
+    		 x.add(r.nextInt(6)+1);
+    	 }
+    	 
+    	 for(int i=0;i<b;i++) {
+    		 Random r = new Random();
+    		 y.add(r.nextInt(6)+1);
+    	 }
+    	 
+    	 Collections.sort(x);
+    	 Collections.sort(y);
+    	 Collections.reverse(x);
+    	 Collections.reverse(y);
+    	 
+    	 if(x.get(0) != y.get(0)) {
+    		 return x.get(0) > y.get(0);
+    	 }
+    	 
+    	 if(y.size() > 1) {
+    		 if(x.get(1) != y.get(1)) {
+    			 return x.get(1) > y.get(1);
+    		 }
+    	 }
+    	 
+    	 return false;
+     }
+     public abstract void distribute_soldiers(int k,player opponent,int mapSz) ; /// strategy depends on the agent
+     
+     public abstract void simulate_attack(player opponent,int mapSz) ; /// strategy depends on agent
+    
+     public abstract void startAttack(player opponent,int mapSz); // in case we want to make more than one attack
+     
+}
---
+++ src/risk/Greedy.java
@@ -0,0 +1,113 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package risk;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import javafx.scene.paint.Color;
+
+/**
+ *
+ * @author Nevine
+ */
+public class Greedy extends AIPlayer {
+    Greedy(){ /// empty constructor
+		territories = new ArrayList<Integer>();
+		soldiers_of_each_territory = new int[55];
+	}
+	
+    Greedy(ArrayList<Integer> a , int all[] , int total , Color co){
+    	territories = a;
+    	turn = false;
+    	for(int i=0;i<55;i++) {
+    		soldiers_of_each_territory[i] = all[i];
+    		soldiers = total;
+    		c = co;
+    	}
+    	
+    }
+
+    public void distribute_soldiers(int k, player opponent, int mapSz) {
+        //get neighbors, sum BST of neighbors
+        int[] BST = null;
+        double[] BSR = null;
+        double sumBSR = 0, maxNBSR = 0;
+        double[] NBSR = null;
+        int[] stateIndex = null;
+        int BSTsz = 0;
+        int count = 0;
+        // Goal -> get Most NBSR of All territories i have
+        // to get BSR w have to find (number of troops of each neighbours) / (number of troops in my territory) of each territory i have
+        // in countriesRatios we can save
+        // NBSR
+        int countriesRatios[][] = new int[27][27];
+        for (int i = 0; i < territories.size(); i++) {
+            ArrayList<Integer> neighbours = AdjacentyMatrix.getNeighbours(territories.get(i), mapSz);
+            ArrayList<Integer> ellyGnby = new ArrayList<>();
+            for (int j = 0; j < neighbours.size(); j++) { //for each country, find neighbors
+                if (isMine(neighbours.get(j))) //lw heya el neighbor bta3y msh 3ayezha
+                {
+                    continue;
+                }
+
+                count += opponent.soldiers_of_each_territory[neighbours.get(j)];
+                if (BSTsz == 0) { //first element
+                    BST[0] = count;
+                    BSR[0] = BST[0] / (soldiers_of_each_territory[neighbours.get(j)]);
+                    sumBSR += BSR[0];
+                    stateIndex[0] = territories.get(j);
+
+                } else {
+                    BSTsz += 1;
+                    BST[BSTsz] = count; //array of BST for each state
+                    BSR[BSTsz] = BST[BSTsz] / (soldiers_of_each_territory[neighbours.get(j)]);
+                    sumBSR += BSR[BSTsz];
+                    stateIndex[BSTsz] = territories.get(j);
+
+                }
+
+            }
+
+        }
+        // 1. Array <Ratios> -> index of maximum ration = index of the city
+        double firstMax = -1;
+        double secondMax = -1;
+        int maxIndex = -1;
+        int secondIndex = -1;
+        for (int l = 0; l < BSTsz; l++) {
+            NBSR[l] = BSR[l] / sumBSR; //fill NBSR array 
+        }
+        // {0,5,8,9,6,3} -> My territories Loop from 0 to BSTsz 0 => country 0 , 1 => country 5
+        for (int l = 0; l < BSTsz; l++) {
+            if (NBSR[l] > secondMax) {
+                if (NBSR[l] > firstMax) {
+                    firstMax = NBSR[l];
+                    maxIndex = l;
+                } else {
+                    secondMax = NBSR[l];
+                    secondIndex = l;
+                }
+            } 
+        }
+        double firstRatio = firstMax / (firstMax + secondMax);
+        int firstSoldiers = (int) ((int) k * firstRatio);
+
+        addSoldiers(maxIndex, firstSoldiers);
+        addSoldiers(secondIndex, k - firstSoldiers);
+
+    }
+
+    @Override
+    public void simulate_attack(player opponent, int mapSz) {
+      //in progress
+    }
+
+    @Override
+    public void startAttack(player opponent, int mapSz) {
+		simulate_attack(opponent, mapSz);
+    }
+
+}
